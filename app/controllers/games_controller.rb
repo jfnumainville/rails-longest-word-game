c40d9de03f9257a48e2a5232b8c7164ae6c2d850
Jean-Francois Numainville <jfnumainville@gmail.com>
2022-04-05 21:42:33 -0400

    Added join statement in new action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,7 @@ class GamesController < ApplicationController
   def new
     @lettersArray = []
     10.times{@lettersArray << ('A'..'Z').to_a.sample }
-
+    @lettersArray = @lettersArray.join(" ")
 
   end
 
